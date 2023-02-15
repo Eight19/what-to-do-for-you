e5b0465c057d0ccf09a4cd06e55250118714346d
Tiffany Jones <eight19realty@gmail.com>
2023-02-15 12:27:30 -0500

    fixed app connections

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 // Import the two parts of a GraphQL schema
-const { toDo } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
+  formatError(error) {
+    console.log(error);
+    return error;
+  }
 });
 
 const app = express();
@@ -17,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (toDo,) => {
+const startApolloServer = async () => {
 await server.start();
 server.applyMiddleware({ app });
 
@@ -30,4 +37,4 @@ db.once('open', () => {
 };
 
 // Call the async function to start the server
-startApolloServer(toDo);
+startApolloServer();
